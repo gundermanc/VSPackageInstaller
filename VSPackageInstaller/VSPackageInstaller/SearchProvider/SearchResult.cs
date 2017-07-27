36f6a4172cea18930c580207346a70b5c538808c
Christian Gunderman <chgund@microsoft.com>
2017-07-26 17:43:10 -0700

    Download + invoke installer.

--- VSPackageInstaller/VSPackageInstaller/SearchProvider/SearchResult.cs
+++ VSPackageInstaller/VSPackageInstaller/SearchProvider/SearchResult.cs
@@ -1,8 +1,14 @@
 ﻿namespace VSPackageInstaller.SearchProvider
 {
+    using System;
     using System.Diagnostics;
+    using System.IO;
+    using System.Threading.Tasks;
+    using System.Net;
+    using System.Windows;
     using Microsoft.VisualStudio.Shell.Interop;
     using VSPackageInstaller.Cache;
+    
 
     internal sealed class SearchResult : IVsSearchItemResult
     {
@@ -25,7 +31,33 @@
 
         public void InvokeAction()
         {
-            packageInstaller.InstallPackages(item.ExtensionId.ToString(), item.Title);
+            // TODO: use extension manager integration to allow installation while VS is running.
+            // Gone for now to the wisps of time...
+            // packageInstaller.InstallPackages(item.ExtensionId.ToString(), item.Title);
+
+            Task.Run(InvokeActionAsync);
+        }
+
+        private async Task InvokeActionAsync()
+        {
+            try
+            {
+                var fileName = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid().ToString()}.vsix");
+
+                using (var webClient = new WebClient())
+                {
+                    // TODO: a good citizen would keep a list of known temp artifacts (like this one) and delete it on next start up.
+                    await webClient.DownloadFileTaskAsync(this.item.Installer, fileName);
+                }
+
+                // Use the default windows file associations to invoke VSIXinstaller.exe since we don't know the path.
+                Process.Start(new ProcessStartInfo(fileName) { UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                // TODO: perhaps we should handle specific exceptions and give custom error messages.
+                MessageBox.Show(ex.Message);
+            }
         }
 
         public IVsSearchProvider SearchProvider { get; }
